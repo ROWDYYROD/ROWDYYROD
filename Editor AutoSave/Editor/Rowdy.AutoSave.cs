1afe96d8bac48bc3adcbe050c06e291941f713c5
ROWDYROD <153943366+ROWDYYROD@users.noreply.github.com>
2025-01-16 10:00:06 +0000

    Initial EditorAutoSave

---
+++ Editor AutoSave/Editor/Rowdy.AutoSave.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace RowdyRod.EditorTools.AutoSave
+{
+    public class AutoSavePreferences : EditorWindow
+    {
+        [PreferenceItem("Auto Save")]
+        public static void PreferencesGUI()
+        {
+            // Auto-save enable/disable
+            Config.AutoSaveConfig.Enabled.value = EditorGUILayout.Toggle(
+                "Enable Auto Save", 
+                Config.AutoSaveConfig.Enabled.value
+            );
+
+            // Save interval
+            Config.AutoSaveConfig.Delay.value = EditorGUILayout.FloatField(
+                "Save Interval (seconds)", 
+                Config.AutoSaveConfig.Delay.value
+            );
+
+            // Save preferences
+            if (GUI.changed)
+            {
+                EditorPrefs.SetBool("AutoSave_Enabled", Config.AutoSaveConfig.Enabled.value);
+                EditorPrefs.SetFloat("AutoSave_Delay", Config.AutoSaveConfig.Delay.value);
+                
+                // Reinitialize the auto-save utility with new settings
+                AutoSaveUtil.Initialize();
+            }
+        }
+    }
+}
---
+++ Editor AutoSave/Editor/Rowdy.Preferences.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace RowdyRod.EditorTools.AutoSave
+{
+    public class AutoSavePreferences : EditorWindow
+    {
+        private static string _saveLocation;
+        private static List<string> _excludedScenes = new List<string>();
+        private static List<string> _excludedAssets = new List<string>();
+
+        [MenuItem("Rowdy Rod/Editor/Auto Save Preferences")]
+        public static void ShowWindow()
+        {
+            GetWindow<AutoSavePreferences>("Auto Save Preferences");
+            _saveLocation = EditorPrefs.GetString("AutoSave_SaveLocation", "");
+            // Load excluded scenes and assets from EditorPrefs (implementation omitted for brevity)
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Auto Save Preferences", EditorStyles.boldLabel);
+            GuiLayout.Label("Made by RowdyRod", EditorStyles.miniLabel);
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Label("Save Location:", GUILayout.Width(100));
+            Event evt = Event.current;
+            Rect dropArea = GUILayoutUtility.GetLastRect();
+            if (dropArea.Contains(evt.mousePosition) && evt.type == EventType.DragUpdated)
+            {
+                DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+                evt.Use();
+            }
+            else if (dropArea.Contains(evt.mousePosition) && evt.type == EventType.DragPerform)
+            {
+                DragAndDrop.AcceptDrag();
+                if (DragAndDrop.paths.Length > 0 && System.IO.Directory.Exists(DragAndDrop.paths[0]))
+                {
+                    _saveLocation = DragAndDrop.paths[0];
+                    EditorPrefs.SetString("AutoSave_SaveLocation", _saveLocation);
+                }
+                evt.Use();
+            }
+            _saveLocation = EditorGUILayout.TextField(_saveLocation);
+            if (GUILayout.Button("Browse", GUILayout.Width(60)))
+            {
+                _saveLocation = EditorUtility.OpenFolderPanel("Select Save Location", _saveLocation, "");
+                EditorPrefs.SetString("AutoSave_SaveLocation", _saveLocation);
+            }
+            EditorGUILayout.EndHorizontal();
+
+            // GUI for managing excluded scenes and assets (implementation omitted for brevity)
+
+            if (GUI.changed)
+            {
+                // Save excluded scenes and assets to EditorPrefs (implementation omitted for brevity)
+            }
+        }
+    }
+}
